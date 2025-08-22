16362e4ab040f1873a2a1d1fe2eec239dd565cb7
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-08-22 13:29:57 -0300

    Toggle the billing cycle price number

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const checkbox = document.querySelector("input[type='checkbox']");
-checkbox.addEventListener("click", (e) => console.log(e.target.checked));
+const monthlyPrices = document.querySelectorAll(".monthly-price");
+const annuallyPrices = document.querySelectorAll(".annually-price");
+
+function toggleBillingCycle() {
+  monthlyPrices.forEach((monthlyPrice) => {
+    monthlyPrice.classList.toggle("hide");
+  });
+
+  annuallyPrices.forEach((annuallyPrice) => {
+    annuallyPrice.classList.toggle("hide");
+  });
+}
+
+checkbox.addEventListener("click", () => {
+  toggleBillingCycle();
+});
